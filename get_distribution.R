77b97874312b0c4ae15a43706f9f7256a158baa6
masalmon <maelle.salmon@yahoo.se>
2017-06-08 09:50:29 +0200

    fixes grouping (I think?), removes comments, adds dates instead of last month, downloads everything only once

--- get_distribution.R
+++ get_distribution.R
@@ -1,7 +1,5 @@
 library("cranlogs")
-set.seed(1)
 all_packages <- available.packages()
-#some_packages <- sample(all_packages[,1], size = 1000)
 
 
 get_downloads <- function(package){
@@ -9,24 +7,26 @@ get_downloads <- function(package){
     package[grep("redland", package)] <- "redland"
   }
   data <- try(cranlogs::cran_downloads(package,
-                           when = "last-month"))
+                                       from = "2017-05-07",
+                                       to = "2017-06-03"))
   if(methods::is(data, "try-error")){
     print(package)
     return(NULL)
   }else{
+    # get no. per week
     data <- dplyr::mutate_(data, week = lazyeval::interp(quote(lubridate::week(date))))
-    data <- dplyr::group_by_(data, c("package", "week"))
-    #data <- dplyr::filter_(data, lazyeval::interp(quote(n() == 7)))
+    dots <- lapply(c("package", "week"), as.symbol)
+    data <- dplyr::group_by_(data, .dots = dots)
     data <- dplyr::summarise_(data, count = lazyeval::interp(quote(sum(count))))
-    return(data)
-    #count <- median(data$count)
-    # return(data.frame(package = package,
-    #                   no_downloads = count))
+    
+    # get median over 4 weeks
+    data <- dplyr::group_by_(data, "package")
+    data <- dplyr::summarise_(data, count = lazyeval::interp(quote(stats::median(count))))
+    return(data)                  
   }
   
 }
 
-#downloads <- purrr::map_df(some_packages, get_downloads)
 all_packages <- unname(all_packages[,1])
 sp <- split(all_packages, ceiling(seq_along(all_packages)/100))
 downloads <- purrr::map_df(sp, get_downloads)
@@ -35,13 +35,10 @@ downloads <- purrr::map_df(sp, get_downloads)
 ropensci <- jsonlite::fromJSON("https://raw.githubusercontent.com/ropensci/roregistry/master/registry.json")
 ropensci_packages <- ropensci$packages
 ropensci_packages <- dplyr::filter(ropensci_packages, on_cran, !cran_archived)
-#ropensci_downloads <- purrr::map_df(ropensci_packages$name, get_downloads)
-ropensci_downloads <- get_downloads(ropensci_packages$name)
-ropensci_downloads <- dplyr::mutate(ropensci_downloads, ropensci = TRUE)
 
-downloads <- dplyr::filter(downloads, !package %in% ropensci_packages$name)
-downloads <- dplyr::mutate(downloads, ropensci = FALSE)
-downloads <- dplyr::bind_rows(downloads, ropensci_downloads)
+
+downloads <- dplyr::mutate(downloads, ropensci = (package %in% ropensci_packages$name))
+
 
 readr::write_csv(downloads, path = "downloads.csv")
 
